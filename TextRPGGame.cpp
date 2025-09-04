aa5bdea67f4972b2c23ade639eb78c04dbf217f3
Dong Bin <seodongb0807@gmail.com>
2025-09-04 20:21:50 +0900

    Feat: Add Game Manager
    게임 매니저와 main 함수 뼈대 구성

--- TextRPGGame.cpp
+++ TextRPGGame.cpp
@@ -1,20 +1,62 @@
 ﻿// TextRPGGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <string>
 #include <iostream>
+#include <vector>
+#include "GameManager.h"
+
+using namespace std;
+
+string nickname;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "게임을 시작합니다." << endl;
+
+    cout << "닉네임을 입력해 주세요(공백 불가): "; //공백 입력시 예외 처리 추가 필요
+    cin >> nickname;
+
+    Character* player = new Character(nickname);        //delete 필수
+
+    while (true)
+    {
+        if (player->getLevel() != 10) //10레벨이 아닐 경우
+        {
+            try
+            {
+                //전투 상황
+                Monster* monster = GameManager::generateMonster(player->getLevel);
+
+                GameManager::battle(player, monster);
+            }
+            catch (const runtime_error& e)
+            {
+                cout << e.what() << endl; //플레이어 사망시 프로그램 종료
+            }
+        
+            //전투 종료 후 상점 방문
+            GameManager::visitShop(player);
+        }
+        else //10레벨 도달 후 보스전 진입
+        {
+            try
+            {
+                //전투 상황
+                Monster* monster = GameManager::generateBossMonster(); //보스 몹 생성
+
+                GameManager::battle(player, monster);
+
+                cout << "보스 몬스터를 처치했습니다!" << endl;
+                break;
+            }
+            catch (const runtime_error& e)
+            {
+                cout << e.what() << endl; //플레이어 사망시 프로그램 종료
+            }
+        }
+
+    }
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
