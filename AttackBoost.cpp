a84d4090516f49a88598bb001a55c948847cdc3c
Veritus <tls9603@gmail.com>
2025-09-05 12:12:01 +0900

    add item,HealthPotion,AttackBoost

---
+++ AttackBoost.cpp
@@ -0,0 +1,17 @@
+﻿#include "AttackBoost.h"
+#include "Character.h"
+#include <iostream>
+
+using namespace std;
+
+// 생성자: 아이템 이름과 공격력 증가량을 초기화한다.
+AttackBoost::AttackBoost()
+    : Item("공격력 부스트"), attackIncrease(10)
+{
+}
+// 기능 구현: 캐릭터의 increaseAttack 함수를 호출하여 공격력을 증가시킨다.
+void AttackBoost::use(Character* character)
+{
+    cout << character->getName() << "이(가) " << getName() << "을(를) 사용합니다." << endl;
+    character->increaseAttack(attackIncrease);
+}
